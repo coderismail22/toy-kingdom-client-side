deaacd07bdf73101424dceac478032955af1a6bc
coderismail22 <122710226+coderismail22@users.noreply.github.com>
2023-06-18 00:52:21 +0600

    custom-hooks-added

---
+++ src/hooks/useToyData.jsx
@@ -0,0 +1,15 @@
+// import React, { useState } from 'react';
+
+// const useToyData = () => {
+//     const [allItems, setAllItems] = useState([]);
+//     const [loading,setLoading] = useState(true);
+//     fetch("menu.json")
+//       .then((res) => res.json())
+//       .then((data) =>{
+//         setAllItems(data);
+//         setLoading(false)
+//       })
+//     return [allItems,loading];
+// };
+
+// export default useToyData;
